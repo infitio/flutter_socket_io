62a36b8529d42b727b93ef050acd17f48a6bc758
Soumya <soumya@infitio.com>
2018-11-21 17:38:40 +0530

    enhancing query params...

--- lib/manager.dart
+++ lib/manager.dart
@@ -10,7 +10,7 @@ class SocketIOManager {
 
   Map<int, SocketIO> sockets = {};
 
-  Future<SocketIO> createInstance(String uri, {Map query}) async {
+  Future<SocketIO> createInstance(String uri, {Map<String, String> query}) async {
     int index = await _channel
         .invokeMethod('newInstance', {'uri': uri, 'query': query});
     SocketIO socket = SocketIO(index);
