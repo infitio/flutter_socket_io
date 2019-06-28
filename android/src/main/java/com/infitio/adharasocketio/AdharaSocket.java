17cc4be483cebdeab516f609797a5d859ebc625c
kim.jong.rok <kim.jong.rok@danbiedu.co.kr>
2019-06-28 11:52:16 +0900

    오류 수정

--- android/src/main/java/com/infitio/adharasocketio/AdharaSocket.java
+++ android/src/main/java/com/infitio/adharasocketio/AdharaSocket.java
@@ -136,7 +136,7 @@ class AdharaSocket implements MethodCallHandler {
                 socket.emit(eventName, data,  new Ack() {
                     @Override
                     public void call(Object... args) {
-                        Log.d(TAG, args);
+                        Log.d(TAG, 'ack come');
                     }
                 });
                 result.success(null);
