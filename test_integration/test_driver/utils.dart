19792b6611fa09338049c14868591f876baa06e8
Rohit R. Abbadi <tiholic@users.noreply.github.com>
2021-04-03 12:57:44 +0530

    test ip address

--- test_integration/test_driver/utils.dart
+++ test_integration/test_driver/utils.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 Future<String> getIP() async {
+  print('addresses:: ${NetworkInterface.list()}');
+
   for (final interface in await NetworkInterface.list()) {
     return (interface.addresses
                 .firstWhere((address) => address.address.startsWith('192.')) ??
